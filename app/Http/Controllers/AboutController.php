6411d6e218be877e96c8666ac6a48a61632ba4e1
anaradiocta <nelaocta12@gmail.com>
2024-09-04 21:42:11 +0700

    Jobsheet 02 - Routing, Controller, Dan View

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller{
+    public function about(){
+        return 'Nama : Anaradi Octa Lavechia <br> NIM : 2241760007';
+    }
+}
+?>
---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class PageController extends Controller
+{
+    public function index() {
+        return 'Selamat Datang!';
+    }
+
+    public function about() {
+        return 'Nama : Anaradi Octa Lavechia <br> NIM : 2241760007';
+    }
+
+    public function articles($id) {
+        return 'Halaman Artikel dengan ID ' .$id;
+    }
+}
+?>
