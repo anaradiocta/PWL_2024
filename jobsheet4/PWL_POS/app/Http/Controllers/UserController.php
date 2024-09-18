77518d34146921b8662f6aa090b4959421232bea
anaradiocta <nelaocta12@gmail.com>
2024-09-18 21:01:12 +0700

    Jobsheet 04 - Model & Eloquent ORM

--- jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
+++ jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,16 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller{
     public function index()
     {
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); //tambahkan data ke tabel m_user
-
-        $user = UserModel::all(); //mengambil semua data dari table m_user
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user',['data'=>$user]);
     }
 }
