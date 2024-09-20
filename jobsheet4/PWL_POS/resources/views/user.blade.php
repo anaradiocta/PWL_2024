fb39eb34ab191db069c41dfbb164e7bcebcc4dfc
anaradiocta <nelaocta12@gmail.com>
2024-09-20 18:27:39 +0700

    Jobsheet 04 - Model & Eloquent ORM

--- jobsheet4/PWL_POS/resources/views/user.blade.php
+++ jobsheet4/PWL_POS/resources/views/user.blade.php
@@ -4,13 +4,15 @@
         <title>Data User</title>
     </head>
     <body>
-        <h1>Data User<h1>
+        <h1>Data User</h1>
+        <a href="/PWL_2024/jobsheet4/PWL_POS/public/user/tambah">+ Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level pengguna</th>
+                <th>Aksi</th>
             </tr>
             @foreach ($data as $d)
             <tr>
@@ -18,7 +20,9 @@
                 <td>{{  $d->username }}</td>
                 <td>{{  $d->nama }}</td>
                 <td>{{  $d->level_id }}</td>
-                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+                <td>{{  $d->level->level_kode   }}</td>
+                <td>{{  $d->level->level_nama   }}</td>
+                <td><a href="/PWL_2024/jobsheet4/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/PWL_2024/jobsheet4/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a></td>
             </tr>
             @endforeach
         </table>
